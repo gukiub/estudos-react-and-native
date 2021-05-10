9f8ba78aff6bd50e248fa0766eab793f7c6e0cc5
Gustavo Lafant <gols@gft.com>
2021-05-10 09:18:44 -0300

    Correção build com erro

--- React-native/marketPlace/src/pages/Cart/styles.js
+++ React-native/marketPlace/src/pages/Cart/styles.js
@@ -1,7 +1,20 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
     flex: 1;
-    padding: 30px;
-    background: #333;
+    align-items: center;
+`;
+
+export const ProductContainer = styled.View`
+    flex: 1;
+    flex-direction: row;
+    margin-top: 60px;
+    border-radius: 20px;
+    background-color: #e83f5b;
+`;
+
+export const ProductList = styled(FlatList)`
+    flex: 1;
+    padding: 30px 10px;
 `;
